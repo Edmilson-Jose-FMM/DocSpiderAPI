e394b69651425ccbfc778afa09e44520c8923527
Edmilson Marques <junior.ejbm@gmail.com>
2025-07-13 22:00:12 -0300

    merge

--- DocSpider.Infra/Context/SpiderDbContext.cs
+++ DocSpider.Infra/Context/SpiderDbContext.cs
@@ -6,7 +6,6 @@ namespace DocSpider.Infra.Context
 {
     public class SpiderDbContext : DbContext
     {
-        public SpiderDbContext(DbContextOptions<SpiderDbContext> options) : base(options) { }
         public DbSet<Documents> Documents { get; set; }
         public SpiderDbContext(DbContextOptions options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
